21f214af5848fd2808e7c2cb268ec38f4e863331
Judah Daniels <judahdaniels@outlook.com>
2022-11-14 10:00:40 +0000

    pipeline from data to Path for parsing

--- app/MainCSVTest.hs
+++ app/MainCSVTest.hs
@@ -1,20 +1,20 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeSynonymInstances #-}
 
-import Control.Applicative
-import qualified Data.ByteString.Lazy as BL
+import Common
+import HeuristicParser
+import PVGrammar
+
 import Data.Csv
 import Data.List.Split
--- Musicology imports
-import qualified Data.Map.Strict as DM
 import Data.Maybe
-import Data.Ratio
+
 import qualified Data.Vector as V
+import qualified Data.ByteString.Lazy as BL
 import qualified Musicology.Core as Music
-import qualified Musicology.Core.Slicing as Music
-import qualified Musicology.MusicXML as MusicXML
+
 import Musicology.Pitch
   ( Interval,
     MidiInterval,
@@ -25,9 +25,7 @@ import Musicology.Pitch
     SPitch,
     pc,
   )
-import Musicology.Pitch.Spelled as MT
-
-type IntervalType = (Ratio Int, Ratio Int)
+import PVGrammar.Parse (protoVoiceEvaluator)
 
 instance FromField Music.RightTied where
   parseField s = case s of
@@ -73,25 +71,90 @@ instance FromNamedRecord ChordLabel where
 
 main :: IO ()
 main = do
-  slices <- slicesFromFile "preprocessing/salamis.csv"
+  slices <- slicesFromFile' "preprocessing/salamis.csv"
   chords <- chordsFromFile "preprocessing/chords.csv"
 
-  printSlicesWithLabels slices chords
+  path <- pure $ pathFromSlices protoVoiceEvaluator slices  
+  print  path
 
--- | Prints out the chords and corresponding slices for debugging.
-printSlicesWithLabels :: [[Slice]] -> [Chord] -> IO ()
-printSlicesWithLabels slices chords = do
+  printSlicesWithLabels' slices chords
+
+pathFromSlices 
+  :: forall ns 
+  .  Eval (Edges ns) [Edge ns] (Notes ns) [ns] (PVLeftmost ns)
+  -> [ ([(ns, Music.RightTied)], Bool)] 
+  -> Path (Maybe [Edge ns], Bool) (Slice (Notes ns)) 
+pathFromSlices eval = reversePath . mkPath (Nothing, False) 
+  where
+    mkPath 
+      :: (Maybe [Edge ns], Bool) 
+      -> [([(ns, Music.RightTied)], Bool)] 
+      -> Path (Maybe [Edge ns], Bool) (Slice (Notes ns)) 
+
+    mkPath eLeft ((slice,boundary):rst) = Path eLeft (Slice $ evalSlice' (map fst slice)) $ mkPath (Just $ getTiedEdges slice, boundary) rst
+    mkPath eLeft [] = PathEnd (Nothing, False)
+    
+    evalSlice' :: [ns] -> Notes ns 
+    evalSlice' = evalSlice eval
+
+    getTiedEdges :: [(ns, Music.RightTied)] -> [Edge ns]
+    getTiedEdges  = mapMaybe mkTiedEdge 
+      where   
+        mkTiedEdge :: (ns, Music.RightTied) -> Maybe (Edge ns)
+        mkTiedEdge (_, Music.Ends) = Nothing
+        mkTiedEdge (pitch, Music.Holds) = Just (Inner pitch, Inner pitch)
+    
+    
+printSlicesWithLabels' :: [(Slice', Bool)] -> [Chord] -> IO ()
+printSlicesWithLabels' slices chords = do
   mapM_ (printLine chords slices) [0 .. 10]
   where
-    printSlice :: Slice -> IO ()
+    printSlice :: Slice' -> IO ()
     printSlice slice = do
       print slice
 
-    printSegment :: [Slice] -> IO ()
+    printSegment :: [(Slice', Bool)] -> Int -> IO ()
+    printSegment segment i = do
+      f segment i
+      where 
+        f [] _ = pure ()
+        f _ (-1) = pure ()
+
+        f ((s,b):sx) i = do
+          if b then 
+            if i == 1 then 
+              do 
+                print s
+                f sx (i-1)
+            else 
+              f sx (i-1)
+          else
+            if i == 0 then 
+              do 
+                print s 
+                f sx i
+            else 
+              f sx i
+
+    printLine :: [Chord] -> [(Slice',Bool)] -> Int -> IO ()
+    printLine chords slices i = do
+      putStrLn $ chords !! i <> ": "
+      printSegment slices (i+1)  
+
+-- | Prints out the chords and corresponding slices for debugging.
+printSlicesWithLabels :: [[Slice']] -> [Chord] -> IO ()
+printSlicesWithLabels slices chords = do
+  mapM_ (printLine chords slices) [0 .. 10]
+  where
+    printSlice :: Slice' -> IO ()
+    printSlice slice = do
+      print slice
+
+    printSegment :: [Slice'] -> IO ()
     printSegment segment = do
       mapM_ printSlice segment
     -- mapM_ (show) [0 .. (DM.size segment)]
-    printLine :: [Chord] -> [[Slice]] -> Int -> IO ()
+    printLine :: [Chord] -> [[Slice']] -> Int -> IO ()
     printLine chords slices i = do
       putStrLn $ chords !! i <> ": "
       printSegment (slices !! i)
@@ -111,14 +174,11 @@ chordsFromFile file = do
     parseChordLabel r = _chord r
 
 -- | Datatype for a slice
-type Slice = [(SPitch, Music.RightTied)]
-
--- [(SPitch, RightTied, SegmentID, sliceId)] -> []
+type Slice' = [(SPitch, Music.RightTied)]
 
 -- | Loads slices from filepath
--- slicesFromFile :: FilePath -> IO (DM.Map Int (DM.Map Int Slice))
-slicesFromFile :: FilePath -> IO [[Slice]]
-slicesFromFile file = do
+slicesFromFile' :: FilePath -> IO [(Slice', Bool)]
+slicesFromFile' file = do
   txt <- BL.readFile file
   case decodeByName txt of
     Left err -> pure []
@@ -127,22 +187,19 @@ slicesFromFile file = do
           segmentedNotes :: [[(SPitch, Music.RightTied, Bool, Bool)]]
           segmentedNotes = splitWhen (\(_, _, newSeg, _) -> newSeg) notes
           segmentedNotes' = (map . map) (\(s, t, _, n) -> (s, t, n)) segmentedNotes
-          segmentedSlices :: [[Slice]]
+          -- segmentedSlices :: [(Slice, Bool)]
           segmentedSlices = map (((map . map) (\(s, t, _) -> (s, t))) . splitWhen (\(_, _, newSlice) -> newSlice)) segmentedNotes'
-      pure segmentedSlices
+          b = output segmentedSlices
+      pure b
+          where
+            output :: [[[(SPitch,Music.RightTied)]]] -> [(Slice', Bool)]
+            -- f :: [[a]] -> (a->b) -> [b]
+            output = concatMap addBoundaries 
 
--- where
-
--- segmentNotes (sPitch, tied, segmentId, sliceId) = (segmentId, [(sPitch, tied, sliceId)])
-
---         segmentedNotesList = segmentNotes <$> notes
---         segmentedNotes = DM.fromAscListWith (++) segmentedNotesList
---         segmentedSlices = DM.map mapFromSlices segmentedNotes -- Combine slices within segments
---     pure segmentedSlices
--- where
---   segmentNotes (sPitch, tied, segmentId, sliceId) = (segmentId, [(sPitch, tied, sliceId)])
---   mapFromSlices segment = DM.fromAscListWith (++) (segmentSlices <$> segment)
---   segmentSlices (sPitch, tied, sliceId) = (sliceId, [(sPitch, tied)])
+            -- Assign boundary marker to first slice of each segment
+            addBoundaries :: [[(SPitch, Music.RightTied)]] -> [(Slice', Bool)]
+            addBoundaries [] = []
+            addBoundaries (s:sx) = (s, True):map (\s -> (s, False)) sx
 
 -- Parse a salami note as output from the python salalmis package.
 noteFromSalami :: SalamiNote -> (SPitch, Music.RightTied, Bool, Bool)
