f9501b0575f4892a615ab0aa9376f1014bdb645e
Judah Daniels <judahdaniels@outlook.com>
2023-04-20 01:57:38 +0100

    d

--- app/MainFullParse.hs
+++ app/MainFullParse.hs
@@ -122,11 +122,7 @@ main = Log.withStderrLogging $ do
   inputSlices <- slicesFromFile' (inputPath <> "slices/" <> corpus <> "/" <> pieceName <> ".csv")
   let outputFile = outputPath <> corpus <> "/" <> pieceName <> "/" <> showRoot algo <> "/" <> expId <> ".json"
   let outputFileDeriv = outputPath <> corpus <> "/" <> pieceName <> "/" <> showRoot algo <> "/" <> expId <> "_deriv"
-  createDirectory $ outputPath 
-  createDirectory $ outputPath <> corpus 
-  createDirectory $ outputPath <> corpus <> "/" <> pieceName 
-  createDirectory $ outputPath <> corpus <> "/" <> pieceName <> "/" <> showRoot algo 
-  createDirectory $ outputPath <> corpus <> "/" <> pieceName <> "/" <> showRoot algo <> "/" <> expId 
+  createDirectoryIfMissing True $ outputPath <> corpus <> "/" <> pieceName <> "/" <> showRoot algo <> "/"
 
   res <- replicateM iterations $ runAlgo outputFileDeriv algo timeOut inputChords inputSlices numRetries
 
