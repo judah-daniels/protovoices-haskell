3bac95a2affba30c1d9148a7ce57754af9e15f56
Judah Daniels <judahdaniels@outlook.com>
2023-05-03 00:37:20 +0100

    upload informed reduction

---
+++ src/Algorithm/InformedReduction.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Algorithm.InformedReduction 
+  (informedReduction)
+    where
+
+import Musicology.Core
+import Internal.MultiSet qualified as MS
+import PVGrammar
+
+import FileHandling 
+import Harmony.ChordLabel
+import Harmony
+
+informedReduction
+  :: [[InputSlice SPitch]]
+  -> [ChordLabel]
+  -> [Notes SPitch]
+informedReduction  inputSlices chords = genSlicePerfect <$> zip inputSlices chords
+ where 
+  filterNote lbl note = 
+    let chordToneProfile' = chordToneProfile $ chordType lbl
+        noteRelRoot = transposeNote (rootNote lbl) note
+      in 
+        fifths noteRelRoot `elem` chordToneProfile'
+
+  genSlicePerfect (slcs, lbl) =
+    let allNotes = fst <$> concatMap fst slcs
+        notes = filter (filterNote lbl) allNotes
+      in 
+        Notes $ MS.fromList notes
