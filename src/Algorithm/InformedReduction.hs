6c2c5be92fcb51375cdda755286c66a4ac4680d1
Judah Daniels <judahdaniels@outlook.com>
2023-05-04 21:32:46 +0100

    Fix bug with enharmonics with informed reduction

--- src/Algorithm/InformedReduction.hs
+++ src/Algorithm/InformedReduction.hs
@@ -29,5 +29,23 @@ informedReduction inputSlices chords = genSlicePerfect <$> zip inputSlices chord
   genSlicePerfect (slcs, lbl) =
     let allNotes = fst <$> concatMap fst slcs
         notes = filter (filterNote lbl) allNotes
+        notesl = filter (filterNote (shiftProfileLeft lbl)) allNotes
+        notesr = filter (filterNote (shiftProfileRight lbl)) allNotes
+        profileOptions = [notesl,notes,notesr]
+        withLengths = zip (length <$> profileOptions) profileOptions
       in 
-        Notes $ MS.fromList notes
+       Notes $ MS.fromList (snd $ maximum withLengths)
+    where 
+      enharmonicProfiles lbl = [lbl, shiftProfileLeft lbl, shiftProfileRight lbl]
+
+      shiftProfileLeft lbl@(ChordLabel chordType rootNote) = 
+        let rootNote' = transpose (sic 12) rootNote
+         in 
+          ChordLabel chordType rootNote'
+
+      shiftProfileRight lbl@(ChordLabel chordType rootNote) = 
+        let rootNote' = transpose (sic (-12)) rootNote
+         in 
+          ChordLabel chordType rootNote'
+
+
